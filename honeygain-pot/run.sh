f695fcc93c9d8ef9a72540756d6f2bbbb86fae60
paitoon.j <paitoon.j@cameltire.com>
2025-11-05 16:24:25 +0700

    Add Honeygain Pot add-on with initial configuration and scripts

---
+++ honeygain-pot/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Starting Honeygain Pot Add-on..."
+
+CONFIG_EMAIL=$(jq --raw-output '.email' /data/options.json)
+CONFIG_PASSWORD=$(jq --raw-output '.password' /data/options.json)
+
+export EMAIL="${CONFIG_EMAIL}"
+export PASSWORD="${CONFIG_PASSWORD}"
+
+echo "Email successfully set: ${HG_EMAIL}"
+echo "Password has been set (masked)."
+
+echo "Starting Honeygain Pot application..."
+exec node index.js

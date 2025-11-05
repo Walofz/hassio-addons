817b9ba7342d53e1612ab64d7e4b6d3cafb5efb4
paitoon.j <paitoon.j@cameltire.com>
2025-11-05 16:31:30 +0700

    Refactor environment variable names in run.sh for clarity

--- honeygain-pot/run.sh
+++ honeygain-pot/run.sh
@@ -5,10 +5,10 @@ echo "Starting Honeygain Pot Add-on..."
 CONFIG_EMAIL=$(jq --raw-output '.email' /data/options.json)
 CONFIG_PASSWORD=$(jq --raw-output '.password' /data/options.json)
 
-export EMAIL="${CONFIG_EMAIL}"
-export PASSWORD="${CONFIG_PASSWORD}"
+export HG_EMAIL="${CONFIG_EMAIL}"
+export HG_PASSWORD="${CONFIG_PASSWORD}"
 
-echo "Email successfully set: ${EMAIL}"
+echo "Email successfully set: ${HG_EMAIL}"
 echo "Password has been set (masked)."
 
 echo "Starting Honeygain Pot application..."

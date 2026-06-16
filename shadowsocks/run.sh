394bbe803e18e943f2bc473534a559f17dabfca4
paitoon.j <paitoon.j@outlook.co.th>
2026-06-16 12:00:33 +0700

    Update Shadowsocks configuration and run script to include log level option

--- shadowsocks/run.sh
+++ shadowsocks/run.sh
@@ -3,14 +3,14 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-# ดึงค่าจากหน้าต่าง Configuration
 PASSWORD=$(jq --raw-output '.password' $CONFIG_PATH)
 METHOD=$(jq --raw-output '.method' $CONFIG_PATH)
 DNS_PROVIDER=$(jq --raw-output '.dns_provider' $CONFIG_PATH)
 
-echo "Starting Shadowsocks Server with method: $METHOD and DNS: $DNS_PROVIDER"
+LOG_LEVEL=$(jq --raw-output '.log_level' $CONFIG_PATH)
+export RUST_LOG="shadowsocks=$LOG_LEVEL"
 
-# ตรวจสอบเงื่อนไข: ถ้าเลือก system ไม่ต้องใส่พารามิเตอร์ --dns (ปล่อยให้ระบบจัดการเอง)
+echo "Starting Shadowsocks Server with method: $METHOD and DNS: $DNS_PROVIDER"
 if [ "$DNS_PROVIDER" = "system" ]; then
     exec ssserver -s "0.0.0.0:8388" -m "$METHOD" -k "$PASSWORD"
 else

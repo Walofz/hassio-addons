b94dec50ce4def7441463ec0bfb5cc0bc7a3c5aa
paitoon.j <paitoon.j@outlook.co.th>
2026-06-16 11:39:06 +0700

    Update Shadowsocks add-on to version 1.0.1 with DNS server support

--- shadowsocks/run.sh
+++ shadowsocks/run.sh
@@ -3,9 +3,14 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-# ดึงค่าจากหน้าต่าง Configuration ของ Add-on
+# ดึงค่าจากหน้าต่าง Configuration
 PASSWORD=$(jq --raw-output '.password' $CONFIG_PATH)
 METHOD=$(jq --raw-output '.method' $CONFIG_PATH)
 
-echo "Starting Shadowsocks Server with method: $METHOD"
-exec ssserver -s "0.0.0.0:8388" -m "$METHOD" -k "$PASSWORD"
+# วนลูปอ่านค่า DNS แบบ Multi DNS แล้วสร้างพารามิเตอร์
+DNS_ARGS=""
+for dns in $(jq -r '.dns_servers[]' $CONFIG_PATH); do
+    DNS_ARGS="$DNS_ARGS --dns $dns"
+done
+
+exec ssserver -s "0.0.0.0:8388" -m "$METHOD" -k "$PASSWORD" $DNS_ARGS

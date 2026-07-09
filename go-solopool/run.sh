43eed7c4097ee2ba6e52e978f1b09ae4745bcffe
paitoon.j <paitoon.j@outlook.co.th>
2026-07-09 14:27:35 +0700

    Update config.yaml and run.sh to enable variable difficulty and adjust fixed difficulty settings

--- go-solopool/run.sh
+++ go-solopool/run.sh
@@ -10,6 +10,7 @@ if [ -f "$CONFIG_PATH" ]; then
     export ZMQ_URL=$(jq --raw-output '.ZMQ_URL // empty' $CONFIG_PATH)
     export STRATUM_PORT=$(jq --raw-output '.STRATUM_PORT // empty' $CONFIG_PATH)
     export WEB_PORT=$(jq --raw-output '.WEB_PORT // empty' $CONFIG_PATH)
+    export USE_VARDIFF=$(jq --raw-output '.USE_VARDIFF // empty' $CONFIG_PATH)
     export FIXED_DIFF=$(jq --raw-output '.FIXED_DIFF // empty' $CONFIG_PATH)
     export DISCORD_WEBHOOK_URL=$(jq --raw-output '.DISCORD_WEBHOOK_URL // empty' $CONFIG_PATH)
     export WALLET_ADDRESS=$(jq --raw-output '.WALLET_ADDRESS // empty' $CONFIG_PATH)
